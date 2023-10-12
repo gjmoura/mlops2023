114b6e71654a1197b51bba277a95de89125fe431
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-10-12 16:59:46 -0300

    Adiciona debug tratamento

--- Python_Essentials_for_MLOps/Project_02/podcast_summary.py
+++ Python_Essentials_for_MLOps/Project_02/podcast_summary.py
@@ -1,7 +1,7 @@
 import os
-import json
 import requests
 import xmltodict
+import logging
 
 from airflow.decorators import dag, task
 import pendulum
@@ -18,7 +18,10 @@ EPISODE_FOLDER = "episodes"
     catchup=False,
 )
 def podcast_summary():
-    create_database = SqliteOperator(
+    """
+    Initial execution for podcast summary
+    """
+    create_podcast_database = SqliteOperator(
         task_id='create_table_sqlite',
         sql=r"""
         CREATE TABLE IF NOT EXISTS episodes (
@@ -34,18 +37,30 @@ def podcast_summary():
     )
 
     @task()
-    def get_episodes():
-        data = requests.get(PODCAST_URL)
-        feed = xmltodict.parse(data.text)
-        episodes = feed["rss"]["channel"]["item"]
-        print(f"Found {len(episodes)} episodes.")
-        return episodes
+    def get_podcast_episodes():
+        """
+        Method to get the list  of podcast episodes
 
-    podcast_episodes = get_episodes()
-    create_database.set_downstream(podcast_episodes)
+        Return: 
+            list: List of podcast episodes
+        """
+        try:
+            data = requests.get(PODCAST_URL)
+            feed = xmltodict.parse(data.text)
+            episodes: list = feed["rss"]["channel"]["item"]
+            print(f"Found {len(episodes)} episodes.")
+            return episodes
+        except requests.exceptions.HTTPError as request_error:
+            logging.error("Error request: %s", request_error)
+
+    podcast_episodes = get_podcast_episodes()
+    create_podcast_database.set_downstream(podcast_episodes)
 
     @task()
-    def load_episodes(episodes):
+    def load_podcast_episodes(episodes: list):
+        """
+        Load a list podcast episodes
+        """
         hook = SqliteHook(sqlite_conn_id="podcasts")
         stored_episodes = hook.get_pandas_df("SELECT * from episodes;")
         new_episodes = []
@@ -56,10 +71,13 @@ def podcast_summary():
 
         hook.insert_rows(table='episodes', rows=new_episodes, target_fields=["link", "title", "published", "description", "filename"])
 
-    load_episodes(podcast_episodes)
+    load_podcast_episodes(podcast_episodes)
 
     @task()
-    def download_episodes(episodes):
+    def download_podcast_episodes(episodes: list):
+        """
+        Download the list of podcast episodes
+        """
         for episode in episodes:
             name_end = episode["link"].split('/')[-1]
             filename = f"{name_end}.mp3"
@@ -70,6 +88,6 @@ def podcast_summary():
                 with open(audio_path, "wb+") as f:
                     f.write(audio.content)
 
-    download_episodes(podcast_episodes)
+    download_podcast_episodes(podcast_episodes)
 
 summary = podcast_summary()
