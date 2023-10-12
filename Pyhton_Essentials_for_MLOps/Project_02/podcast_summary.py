91f3c7128d01b02f247756386ee8dc26277d8448
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-10-12 15:16:07 -0300

    Atualização do código

--- Pyhton_Essentials_for_MLOps/Project_02/podcast_summary.py
+++ Pyhton_Essentials_for_MLOps/Project_02/podcast_summary.py
@@ -8,12 +8,8 @@ import pendulum
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
-from vosk import Model, KaldiRecognizer
-from pydub import AudioSegment
-
 PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
 EPISODE_FOLDER = "episodes"
-FRAME_RATE = 16000
 
 @dag(
     dag_id='podcast_summary',
@@ -59,13 +55,11 @@ def podcast_summary():
                 new_episodes.append([episode["link"], episode["title"], episode["pubDate"], episode["description"], filename])
 
         hook.insert_rows(table='episodes', rows=new_episodes, target_fields=["link", "title", "published", "description", "filename"])
-        return new_episodes
 
-    new_episodes = load_episodes(podcast_episodes)
+    load_episodes(podcast_episodes)
 
     @task()
     def download_episodes(episodes):
-        audio_files = []
         for episode in episodes:
             name_end = episode["link"].split('/')[-1]
             filename = f"{name_end}.mp3"
@@ -75,42 +69,7 @@ def podcast_summary():
                 audio = requests.get(episode["enclosure"]["@url"])
                 with open(audio_path, "wb+") as f:
                     f.write(audio.content)
-            audio_files.append({
-                "link": episode["link"],
-                "filename": filename
-            })
-        return audio_files
 
-    audio_files = download_episodes(podcast_episodes)
-
-    @task()
-    def speech_to_text(audio_files, new_episodes):
-        hook = SqliteHook(sqlite_conn_id="podcasts")
-        untranscribed_episodes = hook.get_pandas_df("SELECT * from episodes WHERE transcript IS NULL;")
-
-        model = Model(model_name="vosk-model-en-us-0.22-lgraph")
-        rec = KaldiRecognizer(model, FRAME_RATE)
-        rec.SetWords(True)
-
-        for index, row in untranscribed_episodes.iterrows():
-            print(f"Transcribing {row['filename']}")
-            filepath = os.path.join(EPISODE_FOLDER, row["filename"])
-            mp3 = AudioSegment.from_mp3(filepath)
-            mp3 = mp3.set_channels(1)
-            mp3 = mp3.set_frame_rate(FRAME_RATE)
-
-            step = 20000
-            transcript = ""
-            for i in range(0, len(mp3), step):
-                print(f"Progress: {i/len(mp3)}")
-                segment = mp3[i:i+step]
-                rec.AcceptWaveform(segment.raw_data)
-                result = rec.Result()
-                text = json.loads(result)["text"]
-                transcript += text
-            hook.insert_rows(table='episodes', rows=[[row["link"], transcript]], target_fields=["link", "transcript"], replace=True)
-
-    #Uncomment this to try speech to text (may not work)
-    #speech_to_text(audio_files, new_episodes)
+    download_episodes(podcast_episodes)
 
 summary = podcast_summary()
