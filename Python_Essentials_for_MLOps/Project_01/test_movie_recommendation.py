ef43c12f868680126ede8933a08af84a204de81f
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-10-12 17:03:41 -0300

    Update test

--- Python_Essentials_for_MLOps/Project_01/test_movie_recommendation.py
+++ Python_Essentials_for_MLOps/Project_01/test_movie_recommendation.py
@@ -4,7 +4,7 @@ from movie_recomendation import remove_especial_caracteres_on_string
 
 def test_remove_special_caracteres_from_string() -> None:
     """
-    Testa remoção de caracteres especiais
+    Tests removal of special characters
     """
     assert remove_especial_caracteres_on_string("Teste!@") == "Teste"
     assert remove_especial_caracteres_on_string(
