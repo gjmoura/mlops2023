b363dc910294f2c5ad0e358b2d194334540f1308
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-10-14 14:04:11 -0300

    Update tests

--- Python_Essentials_for_MLOps/Project_01/test_movie_recommendation.py
+++ Python_Essentials_for_MLOps/Project_01/test_movie_recommendation.py
@@ -1,11 +1,24 @@
-# Implementasr testes unitários]
+import os
 from movie_recomendation import remove_especial_caracteres_on_string
+from movie_recomendation import download_data
 
 
 def test_remove_special_caracteres_from_string() -> None:
     """
-    Tests removal of special characters
+    Testa remoção de caracteres especiais
     """
     assert remove_especial_caracteres_on_string("Teste!@") == "Teste"
     assert remove_especial_caracteres_on_string(
         "Star Wars: Threads of Destiny") == "Star Wars Threads of Destiny"
+
+def test_download_data() -> None:
+    """
+    Test the download of data.
+
+    Returns:
+        None
+    """
+    
+    download_data()
+    # Check if the file exists
+    assert os.path.isfile("./ml-25m.zip")
